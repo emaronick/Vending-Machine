3e7503eb5f97aa0924c68b1ca62966b74fc830d6
quinn.weigers <quinn.weigers@MU-C3-03.mullen.pvt.k12.co.us>
2014-10-14 13:35:52 -0600

    commit product

---
+++ Vending Machine/src/Product.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class Product
+
+	{
+	// The name of the snack.
+	private String snackName;
+	public String getSnackName()
+		{
+		return snackName;
+		}
+	// The cost of the snack (in cents).
+	private int costInCents;
+	
+	//public static int[] getPrice()
+	
+	public static void countNumberOfLines() throws IOException
+		{
+	//make counter for how many number of products their are in the vending machine
+	int count = 0;
+	File productList = new File ("snacks.txt");
+	Scanner input = new Scanner (productList);
+	while (input.hasNextLine()) {
+		String line = input.nextLine();
+		count++;
+		}
+	
+	}
+}
